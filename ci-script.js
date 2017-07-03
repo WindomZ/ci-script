65aae0107751dbe635db1d9074e15d0a55f5d459
WindomZ <git.windomz@gmail.com>
2017-07-03 22:53:06 +0800

    feat: implement ci-script cli

--- ci-script.js
+++ ci-script.js
@@ -2,4 +2,66 @@
 /**
  * Created by WindomZ on 17-7-3.
  */
-'use strict';
+"use strict";
+
+const os = require("os");
+
+const exec = require("./exec");
+
+function outputHelp() {
+  process.stdout.write(`
+  Usage: ci-script [options] [dir]
+
+  Execute the CI script.
+
+  Options:
+    -h, --help, help            output usage information
+    -v, -V, --version, version  output the version number
+`);
+}
+
+function outputVersion() {
+  process.stdout.write(require("./package.json").version + os.EOL);
+}
+
+function outputExec(dir) {
+  process.stdout.write(">>> ci-script start..." + os.EOL);
+  exec
+    .exec(dir)
+    .then(() => {
+      process.stdout.write(">>> ci-script complete!" + os.EOL);
+    })
+    .catch(e => {
+      console.error(e.message);
+    });
+}
+
+let argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+  outputExec();
+  return;
+}
+
+let noArgs = true;
+
+argv.every(arg => {
+  if (arg.match(/^(-v|--version|version)$/i)) {
+    outputVersion();
+    noArgs = false;
+    return false;
+  } else if (arg.match(/^(-h|--help|help)$/i)) {
+    outputHelp();
+    noArgs = false;
+    return false;
+  } else if (!arg.startsWith("-")) {
+    outputExec(arg);
+    noArgs = false;
+    return false;
+  }
+  return true;
+});
+
+if (noArgs) {
+  outputHelp();
+}
